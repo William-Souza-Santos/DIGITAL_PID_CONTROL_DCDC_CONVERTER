27e303c9b48087ba41bf0fca4c308268801090f0
William Souza <souzawpessoal2020@gmail.com>
2025-03-01 21:15:57 -0300

    Add RTOS File

---
+++ 02 - Software/03 - RTOS/RTOS/RTOS.ino
@@ -0,0 +1,37 @@
+#include <Arduino_FreeRTOS.h>
+
+void LED_Blink(void *pvParameters);
+void LED_Blink_9(void *pvParameters);
+
+void setup() {
+  xTaskCreate(LED_Blink_13, "Blink13", 128, NULL, 2, NULL);
+  xTaskCreate(LED_Blink_9, "Blink9", 128, NULL, 2, NULL);
+  
+  vTaskStartScheduler();
+}
+
+void loop() {
+  // Not used
+}
+
+void LED_Blink_13(void *pvParameters) {
+  pinMode(13, OUTPUT);
+
+  for (;;) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    digitalWrite(LED_BUILTIN, LOW);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+void LED_Blink_9(void *pvParameters) {
+  pinMode(9, OUTPUT);
+
+  for (;;) {
+    digitalWrite(9, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(500)); // Pisca mais rápido
+    digitalWrite(9, LOW);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
