c3426ca1d6ddfd8a84c9b6949ef6ee935180cb1c
William Souza <souzawpessoal2020@gmail.com>
2025-05-04 10:31:12 -0300

    Schematic final version 04.05.25

--- 02 - Software/02 - Tests and Simulations/01 - Firmware/Protheus_Simulation_Firmware/Protheus_Simulation_Firmware.ino
+++ 02 - Software/02 - Tests and Simulations/01 - Firmware/Protheus_Simulation_Firmware/Protheus_Simulation_Firmware.ino
@@ -21,46 +21,26 @@
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
-
-
-
-
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
 
-const int pwmPin = 9; // Pino para o PWM (OC1A)
+const int pwmPin = 9; // Pino para o PWM
 float DUTY_CYCLE = 0;
-
-bool usarComplementar = true; // false = PWM normal, true = PWM complementar (invertido)
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(pwmPin, OUTPUT);
+  
+  // Configura o Timer1 para gerar PWM de 40 kHz
+  TCCR1A = (1 << COM1A1) | (1 << WGM11);
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10);
+  ICR1 = 1600;  // Frequência de 40 kHz (16 MHz / 1600 = 10 kHz)
+  OCR1A = 1500;  // 50% de duty cycle (800/1600)
 
-  // Configura o Timer1 para gerar PWM de 10 kHz
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
+  DUTY_CYCLE = (int)(((float)OCR1A / ICR1) * 100); // Cálculo correto do duty cycle
 
-  ICR1 = 1600; // TOP = 1600 → 16MHz / 1600 = 10kHz
-  OCR1A = 1000; // 50% de duty cycle
-
- DUTY_CYCLE = (int)(((float)OCR1A / ICR1) * 100); // Cálculo correto do duty cycle
-
-  if (!usarComplementar) {
-    TCCR1A = (1 << COM1A1) |            // OC1A normal (não-invertido)
-             (1 << WGM11);              // Modo PWM rápido com ICR1 como TOP
-  } else {
-    TCCR1A = (1 << COM1A1) | (1 << COM1A0) | // OC1A invertido (complementar)
-             (1 << WGM11);
-  }
-
-  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10); // Prescaler = 1, modo PWM rápido
-
-  //DUTY_CYCLE = ((float)OCR1A / ICR1) * 100;
-
-lcd.begin(16, 2);
+  lcd.begin(16, 2);
   lcd.print("DC DC CONVERTER");
   lcd.setCursor(2, 1);
   lcd.print("BUCK BOOST");  
@@ -73,7 +53,7 @@ void loop() {
   lcd.print("DUTY CYCLE: ");
   lcd.print(DUTY_CYCLE, 1); // Exibe com uma casa decimal
   lcd.print("%  ");
-
+  
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1);
   digitalWrite(LED_BUILTIN, LOW);
